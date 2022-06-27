3ab4663cfb15a08a1e1001b426afaac89485813e
GalihAdhi <galihadhi755@gmail.com>
2022-06-27 09:29:20 +0700

    list pegawai percubaan ke duwa

--- app/Http/Controllers/Admin/PegawaiController.php
+++ app/Http/Controllers/Admin/PegawaiController.php
@@ -6,23 +6,89 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Pegawai;
+use Illuminate\Support\Facades\Hash;
+use App\Models\{Pegawai, RiwayatCuti, Departemen};
 
 class PegawaiController extends Controller
 {
     public function tambahPegawaiHTML(Request $request)
     {
-        return view('admin.pegawai.tambah');
+        
+        $departemen = Departemen::get();
+        return view('admin.pegawai.tambah',compact('departemen'));
+    }
+
+    public function tambahPegawaiDB(Request $request)
+    {
+        $validated = $request->validate([
+            'nama' => 'required',
+            'nik' => 'required',
+            'no_induk' => 'required',
+            'jenis_kelamin' => 'required',
+            'departemen' => 'required',
+            'alamat' => 'required',
+            'password' => 'required'
+        ]);
+
+        $pegawai = Pegawai::create([
+            'nama' => $request['nama'],
+            'nik' => $request['nik'],
+            'no_induk' => $request['no_induk'],
+            'jenis_kelamin'=> $request['jenis_kelamin'],
+            'id_departemen'=> $request['departemen'],
+            'alamat'=> $request['alamat'],
+            'password' => Hash::make($request['password'])
+        ]);
+
+        
+        return redirect()->route('admin.pegawai.list')->with('status', 'Berhasil Menambah Pegawai!');
     }
 
     public function listPegawaiHTML(Request $request)
     {
         $pegawai = Pegawai::get();
-        return view('admin.pegawai.list', compact('pegawai'));
+        $departemen = Departemen::get();
+        return view('admin.pegawai.list', compact('pegawai', 'departemen'));
     }
 
     public function editPegawaiHTML(Request $request, Pegawai $pegawai)
     {
+        
+        return view('admin.pegawai.edit');
+        
+    }
+
+    public function editPegawaiDB(Request $request, Pegawai $pegawai, Departemen $departemen)
+    {
+        $validated = $request->validate([
+            'nama' => 'required',
+            'nik' => 'required',
+            'no_induk' => 'required',
+            'jenis_kelamin' => 'required',
+            'departemen' => 'required',
+            'alamat' => 'required',
+            'password' => 'required'
+        ]);
+        
+        
+        $pegawai->nama = $request['nama'];
+        $pegawai->nik =  $request['nik'];
+        $pegawai->no_induk =  $request['no_induk'];
+        $pegawai->jenis_kelamin =  $request['jenis_kelamin'];
+        $pegawai->id_departemen =  $request['departemen'];
+        $pegawai->alamat =  $request['alamat'];
+        $pegawai->password =  Hash::make($request['password']);
+        $pegawai->save();
+        
+        return redirect()->route('admin.pegawai.list')->with('status', 'Data Berhasil Diperbarui!');
+    }
+
+    public function hapusPegawaiDB(Request $request, Pegawai $pegawai, RiwayatCuti $riwayat_cuti)
+    {
+        RiwayatCuti::where('id_pegawai', $pegawai->id)->delete();
+        $pegawai->delete();
+
+        return redirect()->route('admin.pegawai.list')->with('status', 'Data Berhasil Dihapus!');
         $validated = $request->validate([
             'nama' => 'required|',
             'nik' => 'required',
